92b93db9337fe6eb0e7dac54a110cdb363dc3c02
Isac R Horta <isacbhz@hotmail.com>
2020-04-06 19:12:54 -0300

    Alteracao linha 14

--- ConsoleApp2/ExerciciosDiversos/Program.cs
+++ ConsoleApp2/ExerciciosDiversos/Program.cs
@@ -11,8 +11,7 @@ namespace ExerciciosDiversos
             double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             double desconto = (preco < 20) ? preco * 0.10 : preco * 0.05;
             Console.WriteLine(desconto);
-            Console.WriteLine("Porcentagem desconto: "+desconto);
-            Console.WriteLine("teste commit");
+            Console.WriteLine("Percentual: "+desconto);            
             Console.ReadLine();
         }
     }
